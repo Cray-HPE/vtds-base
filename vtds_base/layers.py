0c88e34de56202909f7e2b99a3dbe675e653315d
Eric Lund <eric.lund@hpe.com>
2024-01-23 19:41:33 -0600

    re-implement layers as loadable modules

--- vtds_base/layers.py
+++ vtds_base/layers.py
@@ -25,59 +25,134 @@
 """
 
 import os
-import sys
+import importlib
+from .errors import ContextualError
 
 
 class Layer:
-    """The class representation of a vTDS layer, used for setting up
-    the python environment and obtaining default configuration and so
-    forth.
+    """The class representation of a vTDS layer, used for calling that
+    layer's API and obtaining that layer's base and test configuration
+    data.
 
     """
-    def __init__(self, layer_root, python_paths, config_path):
-        """Constructor. The 'layer_root' argument is the absolute path
-        at which the root of the layer's contents can be found. The
-        'python_paths' argument is the list of relative paths within
-        the layer at which python modules implementing the layer can
-        be found. The `config_path` argument is the relative path in
-        the layer at which the default config for the layer is found.
+    def __init__(self, module_name):
+        """Constructor. The 'module_name' is used to import the layer
+        and install the layer's API and base configuration implementation.
 
         """
-        cwd = os.getcwd()
-        self.layer_root = layer_root
-        self.python_paths = ["%s/%s" % (path, cwd) for path in python_paths]
-        self.config_path = config_path
+        try:
+            module = importlib.import_module(module_name)
+            self.layer_api_class = module.LayerAPI
+            self.base_config = module.BaseConfig()
+            self.layer_api = None
+        except ImportError as err:
+            raise ContextualError(
+                "cannot import layer module '%s' - %s" % (
+                    module_name, str(err)
+                )
+            ) from err
+        except AttributeError as err:
+            raise ContextualError(
+                "layer module '%s' does not implement a vTDS layer - %s" % (
+                    module_name, str(err)
+                )
+            ) from err
 
-    def install_python_paths(self):
-        """Install the python library search path(s) for the layer into
-        sys.path so that they can be imported as needed.
+    def initialize(self, stack, config, build_dir):
+        """Initialize a layer, setting up its actual implementation
+        based on the stack of layers loaded, the overall fully resolved
+        vTDS configuration and the path to the build directory for the
+        specific layer.
 
         """
-        # Make sure we only add paths that are not already in
-        # 'sys.path'. We could use sets but they aren't ordered, and
-        # order kind of matters.
-        unique_paths = [
-            path
-            for path in self.python_paths
-            if path not in sys.path
-        ]
-        sys.path += unique_paths
+        self.layer_api = self.layer_api_class(stack, config, build_dir)
 
-    def export_python_paths(self):
-        """Export a PYTHONPATH variable containing (at least) the
-        paths found in 'self.python_paths' for use by sub-processes.
+    def get_api(self):
+        """Return the layer API implementation object for use by the
+        caller.
 
         """
-        pythonpath = os.environ.get("PYTHONPATH", "")
-        # Make sure we only populate 'PYTHONPATH' with non-duplicate
-        # paths. We could use sets but they aren't ordered, and order
-        # kind of matters here.
-        pparray = pythonpath.split(':')
-        unique_paths = [
-            path
-            for path in self.python_paths
-            if path not in pparray
-        ]
-        pythonpath += ':' if pythonpath else ""
-        pythonpath += ':'.join(unique_paths)
-        os.putenv("PYTHONPATH", pythonpath)
+        return self.layer_api
+
+
+class FullStack:
+    """The class representation of the full stack of layers:
+    application, cluster, platform and provider.
+
+    """
+    def __init__(
+            self,
+            application=None,
+            cluster=None,
+            platform=None,
+            provider=None
+    ):
+        """Constructor. Stash the Layer objects for each of the layers
+        in a vTDS system here for use by whoever needs access to
+        another layer.
+
+        """
+        self.application = application
+        self.cluster = cluster
+        self.platform = platform
+        self.provider = provider
+
+    def __init_layer__(self, name, layer, config, build_dir):
+        """Initialize a layer where 'name' identifies the name of the
+        layer in the stack (used to sub-divide the build directory
+        tree), 'layer' is the Layer object containing the layer
+        information, 'config' is the full vTDS configuration to be
+        supplied the layer and 'build_dir' is the root of the build
+        directory tree. The build directory tree and sub-directories
+        for each layer are created as needed.
+
+        """
+        if layer is None:
+            # No such layer present, this is not an error, but we
+            # don't want to go any further.
+            return
+        layer_build_dir = os.path.join(build_dir, name)
+        try:
+            os.makedirs(layer_build_dir, 0o700, True)
+        except OSError as err:
+            raise ContextualError(
+                "failed to create build directory for layer "
+                "'%s' ['%s'] - %s" % (name, layer_build_dir, str(err))
+            ) from err
+        layer.initialize(self, config, layer_build_dir)
+
+    def initialize(self, config, build_dir):
+        """Initialize all of the layers rooted at 'build_dir' and
+        supplying the full vTDS configuration found in 'config'. Each
+        layer will have a sub-directory created for it in the build
+        directory tree.
+
+        """
+        self.__init_layer__("application", self.application, config, build_dir)
+        self.__init_layer__("cluster", self.cluster, config, build_dir)
+        self.__init_layer__("platform", self.platform, config, build_dir)
+        self.__init_layer__("provider", self.provider, config, build_dir)
+
+    def get_application(self):
+        """Accessor for the applicaiton layer.
+
+        """
+        return self.application()
+
+    def get_cluster(self):
+        """Accessor for the cluster layer.
+
+        """
+        return self.cluster()
+
+    def get_platform(self):
+        """Accessor for the platform layer.
+
+        """
+        return self.platform()
+
+    def get_provider(self):
+        """Accessor for the provider layer.
+
+        """
+        return self.provider()
